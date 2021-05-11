00b464115ef8aa403bb14a7c19b55e9790b9a49f
Jakub Panek <jakub.panek@educanet.cz>
2021-05-11 12:14:31 +0200

    asi done

---
+++ src/main/java/cz/educanet/Insert.java
@@ -0,0 +1,27 @@
+package cz.educanet;
+import java.util.Scanner;
+import java.sql.*;
+public class Insert {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/luxor", "root",""
+        );
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Add title");
+        String title = sc.nextLine();
+        System.out.println("Add author");
+        String author = sc.nextLine();
+        System.out.println("Add glossary");
+        String glossary = sc.nextLine();
+        System.out.println("Add price");
+        int price = sc.nextInt();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO book VALUES(NULL, ?, ?, ?, ?)");
+        preparedStatement.setString(1, title);
+        preparedStatement.setString(2, author);
+        preparedStatement.setString(3, glossary);
+        preparedStatement.setInt(4, price);
+        preparedStatement.executeUpdate();
+    }
+}
